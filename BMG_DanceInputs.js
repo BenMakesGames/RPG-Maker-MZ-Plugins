f15b7d011b5d5a2b6f82e9c013f3c4680b84957c
Ben <ben@benmakesgames.com>
2024-01-27 16:42:09 -0500

    +1 plugin, and improved readme

---
+++ BMG_DanceInputs.js
@@ -0,0 +1,143 @@
+/*:
+ * @target MZ
+ * @plugindesc Configure "dances" - series of player movements - that trigger events
+ * when performed.
+ * @author Ben Hendel-Doying
+ *
+ * @help Puzzle games like Fez or Tunic feature input sequences that trigger special
+ * events. This plugin lets you configure such sequences in two different ways:
+ *
+ * 1. Using this plugin's parameters, you can configure a dance to trigger a
+ *    common event.
+ * 2. In an event's Note field, enter the text "<dance:___>" replacing "___"
+ *    with a dance sequence (see info below). When the player performs that
+ *    dance, the event's "D" self-switch will be turned on.
+ *
+ * Dances in the plugin parameters and event Note fields must be entered as a
+ * sequence of "u", "d", "l", and "r" characters, representing up, down, left,
+ * and right movement. For example:
+ *
+ *   uuddlrlr
+ *
+ * Represents the moves up, up, down, down, left, right, left, right.
+ *
+ * If the player is unable to perform a move (for example, because they hit a
+ * wall), that move is NOT counted towards the dance.
+ *
+ * Movement via keyboard, gamepad, and mouse/touch are all supported.
+ *
+ * The tag name and dance sequences are case-insensitive. For example, the
+ * following Notes are identical, and both work:
+ *
+ *   <dance:uuddlrlr>
+ *   <DaNce:uUDDlrlR>
+ *
+ * Compatibility:
+ * * EXTENDS Game_Player moveStraight
+ * * EXTENDS DataManager onLoad
+ *
+ * @param maxDanceLength
+ * @type number
+ * @text Maximum Dance Length
+ * @desc The maximum length of a dance, in moves. The default is 100.
+ * @default 100
+ *
+ * @param dances
+ * @type struct<Dance>[]
+ * @text Dances
+ * @desc The dances to configure.
+ * @default []
+ */
+
+/*~struct~Dance:
+ * @param dance
+ * @type text
+ * @text Dance
+ * @desc The input sequence that triggers the common event.
+ * @default
+ *
+ * @param commonEvent
+ * @type common_event
+ * @text Common Event
+ * @desc The common event to trigger when the dance is performed.
+ */
+
+(function() {
+    const pluginName = 'BMG_DanceInputs';
+    const pluginParameters = PluginManager.parameters(pluginName);
+    const maxDanceLength = Number(pluginParameters['maxDanceLength']);
+    const dances = JSON.parse(pluginParameters['dances'])
+        .map(d => JSON.parse(d))
+        .map(d => {
+            return {
+                dance: d.dance.toLowerCase(),
+                commonEvent: Number(d.commonEvent)
+            };
+        });
+
+    // capture player movements in a string
+    let moveHistory = '';
+
+    // check for dances after every player movement
+    const originalGamePlayerMoveStraight = Game_Player.prototype.moveStraight;
+    Game_Player.prototype.moveStraight = function(direction) {
+        originalGamePlayerMoveStraight.call(this, direction);
+
+        switch(direction)
+        {
+            case 2: moveHistory += 'd'; break;
+            case 4: moveHistory += 'l'; break;
+            case 6: moveHistory += 'r'; break;
+            case 8: moveHistory += 'u'; break;
+        }
+
+        if(moveHistory.length > maxDanceLength)
+            moveHistory = moveHistory.substring(moveHistory.length - maxDanceLength);
+
+        checkForDances();
+    };
+
+    function checkForDances()
+    {
+        for(let dance of dances)
+        {
+            if(moveHistory.endsWith(dance.dance))
+                $gameTemp.reserveCommonEvent(dance.commonEvent);
+        }
+
+        for(let event of mapEventsWithDances)
+        {
+            if(moveHistory.endsWith(event.dance))
+                $gameSelfSwitches.setValue([ $gameMap.mapId(), event.eventId, 'D' ], true);
+        }
+    }
+
+    /**
+     * @type {Array<{ dance: string, eventId: number }>}
+     */
+    let mapEventsWithDances = [];
+
+    const originalDataManagerOnLoad = DataManager.onLoad;
+    DataManager.onLoad = function(object) {
+        originalDataManagerOnLoad.call(this, object);
+
+        if (!this.isMapObject(object))
+            return;
+
+        mapEventsWithDances = [];
+
+        for (const event of $dataMap.events.filter(event => !!event))
+            processEvent(event);
+    };
+
+    function processEvent(event)
+    {
+        if (!event.note || !event.note.toLowerCase().includes('<dance:'))
+            return;
+
+        mapEventsWithDances.push({
+            dance: event.note.match(/<dance:(.*)>/i)[1].trim().toLowerCase(),
+            eventId: event.id
+        });
+    }
+})();
--- README.md
+++ README.md
@@ -3,3 +3,13 @@ A collection of RPG Maker MZ plugins I've developed while working on my own game
 Feel free to use them, alter them, take them apart, rearrange them, etc, to suit your needs.
 
 <a href='https://ko-fi.com/A0A12KQ16' target='_blank'><img height='36' style='border:0px;height:36px;' src='https://cdn.ko-fi.com/cdn/kofi3.png?v=3' border='0' alt='Buy Me a Coffee at ko-fi.com' /></a>
+
+### Plugins
+
+* **BMG_CustomCritical** - customize the critical hit damage multiplier
+* **BMG_DanceInputs** - configure sequences of inputs to trigger common events and map events
+* **BMG_IgnoreLuck** - disable the built-in effects of the Luck stat
+* **BMG_NoBasicAttack** - the basic attack move is removed from combat
+* **BMG_Penniless** - the Gold window is removed from the party menu
+* **BMG_ScreenByScreen** - allows players to automatically transition from map to map by moving to the edge of a map
+* **BMG_WeaponlessSkill** - grants players a skill while they are holding no weapon
