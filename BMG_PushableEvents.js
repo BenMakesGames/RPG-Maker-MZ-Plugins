7f456f820bc78761c828d632e553fd138623fb74
Ben H <ben@benmakesgames.com>
2024-01-28 09:53:14 -0500

    +1 plugin, and updated README.md

---
+++ BMG_PushableEvents.js
@@ -0,0 +1,78 @@
+/*:
+ * @target MZ
+ * @plugindesc Add <pushable> to an event's Note field to make it pushable by the player.
+ * @author Ben Hendel-Doying
+ *
+ * @help Put <pushable> in an event's Note field to make it pushable by the player.
+ *
+ * If you'd like pushability to be conditional on a switch and/or item owned by
+ * the party, use the plugin parameters to set a switch and/or item.
+ *
+ * Compatibility:
+ * * EXTENDS Game_Player moveStraight
+ * * ADDS Game_Event isPushable
+ * * ADDS Game_Player canPush
+ * * ADDS Game_Player push
+ *
+ * @param switch
+ * @type switch
+ * @text Required Switch
+ * @desc If this switch is OFF, pushability is disabled.
+ * @default 0
+ *
+ * @param item
+ * @type item
+ * @text Required Item
+ * @desc If the party does not have this item, pushability is disabled.
+ * @default 0
+ */
+(function() {
+    // get parameters
+    const params = PluginManager.parameters('BMG_PushableEvents');
+    const switchId = Number(params['switch']);
+    const itemId = Number(params['item']);
+
+    Game_Event.prototype.isPushable = function()
+    {
+        if(switchId && !$gameSwitches.value(switchId))
+            return false;
+
+        if(itemId && !$gameParty.hasItem($dataItems[itemId], true))
+            return false;
+
+        return this.event().note.toLowerCase().includes('<pushable>');
+    };
+
+    // extend Game_Player
+    const originalGamePlayerMoveStraight = Game_Player.prototype.moveStraight;
+    Game_Player.prototype.moveStraight = function(d) {
+        if(this.canPush(d))
+            this.push(d);
+        else
+            originalGamePlayerMoveStraight.call(this, d);
+    };
+
+    Game_Player.prototype.canPush = function(d) {
+        const x = $gameMap.roundXWithDirection(this.x, d);
+        const y = $gameMap.roundYWithDirection(this.y, d);
+        const events = $gameMap.eventsXy(x, y);
+
+        if(events.length !== 1)
+            return false;
+
+        const event = events[0];
+
+        return event.isPushable();
+    };
+
+    Game_Player.prototype.push = function(d) {
+        const x = $gameMap.roundXWithDirection(this.x, d);
+        const y = $gameMap.roundYWithDirection(this.y, d);
+        const events = $gameMap.eventsXy(x, y);
+        const event = events[0];
+
+        event.moveStraight(d);
+        if(event.isMovementSucceeded())
+            originalGamePlayerMoveStraight.call(this, d);
+    };
+})();
--- README.md
+++ README.md
@@ -2,14 +2,26 @@ A collection of RPG Maker MZ plugins I've developed while working on my own game
 
 Feel free to use them, alter them, take them apart, rearrange them, etc, to suit your needs.
 
+Still using MV? [I've made a bunch of RPG Maker MV plugins, too!](https://github.com/BenMakesGames/RPG-Maker-MV-Plugins)
+
 <a href='https://ko-fi.com/A0A12KQ16' target='_blank'><img height='36' style='border:0px;height:36px;' src='https://cdn.ko-fi.com/cdn/kofi3.png?v=3' border='0' alt='Buy Me a Coffee at ko-fi.com' /></a>
 
 ### Plugins
 
-* **BMG_CustomCritical** - customize the critical hit damage multiplier
-* **BMG_DanceInputs** - configure sequences of inputs to trigger common events and map events
-* **BMG_IgnoreLuck** - disable the built-in effects of the Luck stat
-* **BMG_NoBasicAttack** - the basic attack move is removed from combat
-* **BMG_Penniless** - the Gold window is removed from the party menu
+#### Big
+
+* **BMG_DanceInputs** - configure sequences of movements to trigger common events and map events
+* **BMG_PushableEvents** - allows certain events to be pushed, with optional item and/or switch requirements
 * **BMG_ScreenByScreen** - allows players to automatically transition from map to map by moving to the edge of a map
+
+#### Medium
+
+* **BMG_NoBasicAttack** - the basic attack move is removed from combat
 * **BMG_WeaponlessSkill** - grants players a skill while they are holding no weapon
+
+#### Small
+
+* **BMG_CustomCritical** - customize the critical hit damage multiplier
+* **BMG_IgnoreLuck** - disable the built-in effects of the Luck stat
+* **BMG_Penniless** - the Gold window is removed from the party menu
+
